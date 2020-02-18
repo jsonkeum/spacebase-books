906be413a8e1e351483b91d0a5f17638759954d5
quanchifootball <jasonkeum@gmail.com>
2020-02-18 05:05:26 -0800

    reformatting code

--- spacebase_books/books/views/book.py
+++ spacebase_books/books/views/book.py
@@ -23,43 +23,57 @@ class BookFormView(LoginRequiredMixin, FormView):
     def form_valid(self, form):
         try:
             data = form.cleaned_data
-            if 'book_id' in self.kwargs:
-                book, book_created = Book.objects.update_or_create(pk=self.kwargs['book_id'], defaults={
-                    "title": data["title"],
-                    "author": data["author"]
-                })
+            if "book_id" in self.kwargs:
+                book, book_created = Book.objects.update_or_create(
+                    pk=self.kwargs["book_id"],
+                    defaults={"title": data["title"], "author": data["author"]},
+                )
             else:
-                book, book_created = Book.objects.get_or_create(title=data["title"], author=data["author"])
-            BookUser.objects.update_or_create(user=self.request.user, book=book, defaults={
-                "rating": data["rating"],
-                "external_id": data["external_id"] or None
-            })
+                book, book_created = Book.objects.get_or_create(
+                    title=data["title"], author=data["author"]
+                )
+            BookUser.objects.update_or_create(
+                user=self.request.user,
+                book=book,
+                defaults={
+                    "rating": data["rating"],
+                    "external_id": data["external_id"] or None,
+                },
+            )
             return HttpResponseRedirect(self.get_success_url())
         except IntegrityError as e:
             if book_created:
                 book.delete()
             context = self.get_context_data(**self.kwargs)
-            context['form'] = BookForm({
-                'title': data["title"],
-                'author': data["author"],
-                'rating': data["rating"],
-                'external_id': data["external_id"],
-            })
-            context['error'] = constants.EXTERNAL_ID_ERROR if 'external_id' in str(e) else str(e)
+            context["form"] = BookForm(
+                {
+                    "title": data["title"],
+                    "author": data["author"],
+                    "rating": data["rating"],
+                    "external_id": data["external_id"],
+                }
+            )
+            context["error"] = (
+                constants.EXTERNAL_ID_ERROR if "external_id" in str(e) else str(e)
+            )
             return self.render_to_response(context)
 
 
 class EditBookView(BookFormView):
     def get(self, request, *args, **kwargs):
-        book = get_object_or_404(Book, pk=kwargs['book_id'])
-        book_user = get_object_or_404(BookUser, user=request.user, book_id=kwargs['book_id'])
+        book = get_object_or_404(Book, pk=kwargs["book_id"])
+        book_user = get_object_or_404(
+            BookUser, user=request.user, book_id=kwargs["book_id"]
+        )
         context = self.get_context_data(**kwargs)
-        context['form'] = BookForm({
-            'title': book.title,
-            'author': book.author,
-            'rating': book_user.rating,
-            'external_id': book_user.external_id,
-        })
+        context["form"] = BookForm(
+            {
+                "title": book.title,
+                "author": book.author,
+                "rating": book_user.rating,
+                "external_id": book_user.external_id,
+            }
+        )
 
         return self.render_to_response(context)
 
@@ -72,14 +86,17 @@ def delete_book(request, book_id):
 def export_to_csv(request):
     user_books = get_list_or_404(Book, bookuser__user=request.user)
 
-    response = HttpResponse(content_type='text/csv')
+    response = HttpResponse(content_type="text/csv")
     response[
-        'Content-Disposition'] = f'attachment; filename="books-{request.user.username}-{datetime.datetime.now()}.csv"'
+        "Content-Disposition"
+    ] = f'attachment; filename="books-{request.user.username}-{datetime.datetime.now()}.csv"'
 
     writer = csv.writer(response)
     writer.writerow(["Book ID", "External ID", "Title", "Author", "Rating"])
     for book in user_books:
         details = book.get_user_detail(request.user)
-        writer.writerow([book.id, details.external_id, book.title, book.author, details.rating])
+        writer.writerow(
+            [book.id, details.external_id, book.title, book.author, details.rating]
+        )
 
     return response
--- spacebase_books/books/views/main.py
+++ spacebase_books/books/views/main.py
@@ -15,7 +15,7 @@ class DashboardView(generic.ListView):
     def get_queryset(self):
         books = Book.objects.annotate(
             avg_rating=Coalesce(Avg("bookuser__rating"), 0),
-            reader_count=Count("bookuser")
+            reader_count=Count("bookuser"),
         )
 
         return {
@@ -32,7 +32,9 @@ class MyBooksView(LoginRequiredMixin, generic.ListView):
     template_name = "books/books_list.html"
 
     def get_queryset(self):
-        user_books = Book.objects.filter(bookuser__user=self.request.user).order_by("-created_at")
+        user_books = Book.objects.filter(bookuser__user=self.request.user).order_by(
+            "-created_at"
+        )
         for book in user_books:
             details = book.get_user_detail(self.request.user)
             book.user_rating = details.rating
